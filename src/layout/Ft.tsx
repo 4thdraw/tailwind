6c15b375af0ea4d6a01568c7cd71c0f6b6bdd91d
4thdraw <4thdraw2@gmail.com>
2025-12-22 21:43:38 +0900

    하단반응형

--- src/layout/Ft.tsx
+++ src/layout/Ft.tsx
@@ -8,14 +8,15 @@ export default function Ft() {
 
   return (
     <footer className="ft border-t-2 font-300 mt-[100px]">
-       <div className="max-w-1550 gap-[60px] w-full mx-auto py-[50px] flex justify-between">
-        <div className="logo">
+       <div className="max-w-1550 px-5 gap-[60px] w-full mx-auto py-[50px] flex justify-between">
+        <div className="logo hidden xl:block">
             <img src="/logo.svg" className="w-[150px]"></img>
         </div>
-        <div className="flex-1 flex gap-[50px] font-kr leading-[1.8]">
-            <div className="flex-1">
-                <h3 className="font-700 text-subtitle mb-3">주식회사 포랩코리아</h3>
-                <div className="">
+        <div className="flex-1  grid  grid-cols-12  font-kr leading-[1.8]">
+            <div className="lg:col-span-4  md:col-span-6  col-span-12  companyinfo xl:order-first order-last">
+                <h3 className="font-700 text-subtitle mb-3 flex justify-between border-b-2 md:border-none">주식회사 포랩코리아 
+                  <button className="md:hidden">+</button></h3>
+                <div className="hidden md:block">
                   <span className="font-500">대표 : </span> 강동균<br />
                   <span className="font-500">사업자등록번호 : </span> 578-81-03310 <a href="">[사업자정보확인]</a><br />                
                   <span className="font-500">통신판매업신고번호 : </span> 2024-서울마포-0658 호<br />
@@ -28,8 +29,18 @@ export default function Ft() {
                   <li className="border-r-[1px]"><a href="" className="px-2">개인정보정책</a></li>
                   <li><a href="" className="px-2">이용안내</a></li>                  
                 </ul>
+                {/* 모바일때노출되는  sns */}
+                <div className="snslist  xl:hidden  flex  gap-4 items-end  mt-10">
+                      <a href="" className="bg-black p-3 rounded-full">
+                        <img src="/instar.svg" className="w-[24px]"></img>
+                      </a>
+                      <a href="" className="bg-black p-3  rounded-full">
+                        <img src="/kakao.png" className="w-[24px]"></img>
+                      </a>
+                </div>
             </div>
-            <div className="flex-1">
+            <div className="lg:col-span-8  md:col-span-6 col-span-12   flex flex-col lg:flex-row  lg:gap-[80px]">
+              <div className="custominfo flex-1">
                 <h3 className="font-700 text-subtitle mb-3">고객센터</h3>
                 <div>
                   <span className="font-500">전화</span> 070-4800-3250 평일 10:00~17:00<br />
@@ -37,18 +48,18 @@ export default function Ft() {
                   hyeongwon@fourlab.co.kr
 
                 </div>
-                
-
                 <h3 className="font-700 text-subtitle mb-3 mt-10">BANK INFO</h3>
                 <div>
                   국민은행<br />
                   032901-04-366203<br />
                   예금주 : 주식회사 포랩코리아
                 </div>
-            </div>
-            <div className="flex-1 font-en flex flex-col">
-                <h3 className="font-700 text-subtitle mb-3">HELP</h3>
-                <div>
+              </div>
+              <div className="flex-1 font-en flex flex-col  helpcontent flex-1 mt-10  lg:mt-0">
+                <h3 className="font-700 text-subtitle mb-3 flex justify-between border-b-2 md:border-none">HELP  
+                  <button className="md:hidden">+</button> 
+                </h3>
+                <div className="hidden md:block">
                   <ul>
                     <li>
                       <Link to="/store">매장 안내</Link>
@@ -61,8 +72,8 @@ export default function Ft() {
                     </li>
                   </ul>
                 </div>
-
-                <div className="sns family mt-auto flex justify-between">
+                 {/* 데스크탑에서만 노출되는   sns 패밀리컨텐츠 */}
+                <div className="sns family mt-auto xl:flex hidden justify-between">
                    <div className="snslist flex gap-4 items-end">
                       <a href="" className="bg-black p-3 rounded-full">
                         <img src="/instar.svg" className="w-[24px]"></img>
@@ -94,7 +105,9 @@ export default function Ft() {
                       }
                    </div>
                 </div>
+              </div>                
             </div>
+            
         </div>
        </div>
     </footer>
--- src/layout/Hd.tsx
+++ src/layout/Hd.tsx
@@ -1,28 +1,45 @@
-import { useState, useEffect, use } from 'react';
-import { Link,  useLocation } from 'react-router-dom'
-import { Swiper , SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectFade } from 'swiper/modules';
-import {  Bars3Icon,   XMarkIcon } from "@heroicons/react/16/solid";
+import { useState, useEffect } from 'react'; //컨텐츠변경화면제어
+import { Link,  useLocation } from 'react-router-dom' //주소창관련모듈
+import { Swiper , SwiperSlide } from 'swiper/react'; //슬라이드
+import { Autoplay, EffectFade } from 'swiper/modules'; //슬라이드 옵션
+import {  Bars3Icon,   XMarkIcon } from "@heroicons/react/16/solid"; // ui모듈
 
 
-import 'swiper/css';
+import 'swiper/css'; //슬라이드 스타일
 import 'swiper/css/effect-fade';
 
 export default function Hd() {
 
-  const [scrolled, setScrolled] = useState<boolean>(false);
-  const [ allmenu , setAllmenu ] = useState<boolean>(false);
+  const [scrolled, setScrolled] = useState<boolean>(false); //스크롤판단여부
+  const [ allmenu , setAllmenu ] = useState<boolean>(false); //메뉴여닫힘판단
 
-  const location = useLocation();
-  const isRoot : boolean = location.pathname === "/";
-
+  const location = useLocation(); //주소창분석
+  const isRoot : boolean = location.pathname === "/"; //첫페이지구분
 
   useEffect(() => {
+  setAllmenu(false); //메뉴닫힘
+ }, [location.pathname]); //주소가 바뀔때마다 실행
+
+  useEffect(() => {
+
+    // 스크바의 위치가 50보다 커지면 true, 아니면  false
     const handleScroll = (): void => {
       setScrolled( window.scrollY > 50 ); // 50px 이상 스크롤 시 true
+      setAllmenu(false);
     };
-    window.addEventListener('scroll', handleScroll);
-    return (): void => window.removeEventListener('scroll', handleScroll);
+
+    const handleResizeEvent = () => {
+       setAllmenu(false);
+    };
+
+    window.addEventListener('scroll',  handleScroll );
+    window.addEventListener('resize',  handleResizeEvent );
+
+    return (): void =>{ 
+      window.removeEventListener('scroll',  handleScroll );
+      window.removeEventListener('resize',  handleResizeEvent );
+    }  
+    // 절대 이 아래 식쓰지않기 실행안됨
   }, [])
 
   return (
@@ -56,25 +73,25 @@ export default function Hd() {
                         <div className='flex justify-between align-center px-4 py-[35px]'>
                           <h1 className='flex flex-col items-center justify-center'>
                               <a href="/" className='text-white font-en '>
-                                <img src="/logo.svg" className={`w-[180px]    ${ isRoot ?  scrolled  ?  ""  :  "filter  invert" : "" }` }></img>
+                                <img src="/logo.svg" className={`w-[180px]    ${ isRoot ?  scrolled  ?  ""  :  "filter  invert" : "" } ` }></img>
                               </a>
                           </h1>
-                          <button className='md:hidden w-[36px] text-white' onClick={ () => { setAllmenu(!allmenu) } } >
+                          <button className={`md:hidden w-[36px] ${  isRoot ? scrolled ? "" : "text-white" : "" }`}  onClick={ () => { setAllmenu(!allmenu) } } >
                             {
                               allmenu ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>
                             }
                           </button>
-                          <nav className={` md:block  ${ allmenu ? "" : "hidden" }  md:[position:unset]  absolute top-[100%] 
-                                    w-full bg-black/70 md:bg-transparent start-0 `}>
-                              <ul className={`md:flex gap-6  `}>
+                          <nav className={` md:flex md:justify-end  ${ allmenu ? "" : "hidden" }  md:[position:unset]  absolute top-[100%] 
+                                    w-full bg-black/70 md:bg-transparent start-0 py-8 md:py-0`}>
+                              <ul className={`flex  flex-col  md:flex-row  items-center    md:gap-6  gap-4  `}>
                                   <li>
-                                      <Link to="/about"  className={` ${  isRoot ? scrolled ? "" : "text-white" : "" }  hover:text-main hover:border-b-2 text-navi font-500`} >회사소개</Link>
+                                      <Link to="/about"  className={` ${  isRoot ? scrolled ? "" : "!text-white" : "" } text-white  md:text-black  hover:text-main hover:border-b-2 text-navi font-500`} >회사소개</Link>
                                   </li>
                                   <li>
-                                      <Link to="/product"   className={` ${ isRoot ? scrolled ? "" : "text-white" : "" }  hover:text-main hover:border-b-2 text-navi font-500`} >제품소개</Link>
+                                      <Link to="/product"   className={` ${ isRoot ? scrolled ? "" : "!text-white" : "" } text-white  md:text-black  hover:text-main hover:border-b-2 text-navi font-500`} >제품소개</Link>
                                   </li>
                                   <li>
-                                      <Link to="/board"  className={` ${ isRoot ?  scrolled ? "" : "text-white" : "" }  hover:text-main hover:border-b-2 text-navi font-500`}>커뮤니티</Link>
+                                      <Link to="/board"  className={` ${ isRoot ?  scrolled ? "" : "!text-white" : "" } text-white  md:text-black  hover:text-main hover:border-b-2 text-navi font-500`}>커뮤니티</Link>
                                   </li>                
                               </ul>
                           </nav>
